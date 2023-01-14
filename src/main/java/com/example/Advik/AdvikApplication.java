07860e1d8f80c8d4a04f76d022bfd6dbfecda45d
Sachin Mane <czx2816@varian.com>
2023-01-14 14:39:36 +0530

    hello

---
+++ src/main/java/com/example/Advik/AdvikApplication.java
@@ -0,0 +1,13 @@
+package com.example.Advik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Advik/AdvikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Advik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
